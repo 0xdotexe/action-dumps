7b5c3f4574c6bf9088b24bee73ecc169b7446bd9
Sarvagya Sagar <iamsarvagyaa@gmail.com>
2022-07-07 06:39:42 +0000

    updates: pushed recently build _site folder

---
+++ storage/buildBlog/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Diving in Android Security",
+        "excerpt":"Hello everyone, This is my first blog post. Many of my friends suggested me to get into Android Security. I started learning android security and learned many things, but I faced a lot of problems. So I wanted to create a series of blog posts to guide those peeps who...","categories": ["Android Security"],
+        "tags": ["Android","Beginner","Pentesting"],
+        "url": "https://iamsarvagyaa.github.io/android-security-part-one/"
+      }]
